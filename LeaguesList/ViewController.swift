1d2b91a728b611548423587e32a4ecb21500c4c9
Jason Ngo <jason.n.ngo@gmail.com>
2019-01-22 11:51:19 -0500

    first commit

---
+++ LeaguesList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeaguesList
+//
+//  Created by Jason Ngo on 2019-01-21.
+//  Copyright © 2019 Jason Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
